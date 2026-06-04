3dfbdaa5e5bcb10868d507c32a6b26d016951acb
Dante <dantepeletti@gmail.com>
2026-06-04 00:37:51 -0300

    refactor: migrar base a anotaciones lombok

--- src/main/java/programacion3/entities/Base.java
+++ src/main/java/programacion3/entities/Base.java
@@ -1,65 +1,27 @@
- package programacion3.entities;
+package programacion3.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+
+@Getter
+@SuperBuilder
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
 
 public class Base {
 
     protected Long id;
     protected boolean eliminado;
     protected LocalDateTime createdAt;
-
-    public Base() {
-    }
-
-    public Base(Long id, boolean eliminado, LocalDateTime createdAt) {
-        this.id = id;
-        this.eliminado = eliminado;
-        this.createdAt = createdAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public boolean isEliminado() {
-        return eliminado;
-    }
-
-    public void setEliminado(boolean eliminado) {
-        this.eliminado = eliminado;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    @Override
-    public String toString() {
-        return "Base{" +
-                "id=" + id +
-                ", eliminado=" + eliminado +
-                ", createdAt=" + createdAt +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Base base)) return false;
-        return Objects.equals(id, base.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
