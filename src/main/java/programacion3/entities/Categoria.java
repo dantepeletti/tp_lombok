56ac1b850999ee48efd9e02ad5b6e8ea74f442ea
Dante <dantepeletti@gmail.com>
2026-06-04 02:57:54 -0300

    refactor: migrar DetallePedido a Lombok

--- src/main/java/programacion3/entities/Categoria.java
+++ src/main/java/programacion3/entities/Categoria.java
@@ -1,68 +1,20 @@
 package programacion3.entities;
 
-import java.time.LocalDateTime;
-import java.util.Objects;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString(of = {"nombre","descripcion"})
+@EqualsAndHashCode(of = "nombre",callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
 public class Categoria extends Base {
-
-    private String nombre;
-    private String descripcion;
-    private Set<Producto> productos;
-
-    public Categoria() {
-    }
-
-    public Categoria(Long id, boolean eliminado, LocalDateTime createdAt,
-                     String nombre, String descripcion, Set<Producto> productos) {
-
-        super(id, eliminado, createdAt);
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.productos = productos;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Set<Producto> getProductos() {
-        return productos;
-    }
-
-    public void setProductos(Set<Producto> productos) {
-        this.productos = productos;
-    }
-
-    @Override
-    public String toString() {
-        return "Categoria{" +
-                "nombre='" + nombre + '\'' +
-                ", descripcion='" + descripcion + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Categoria that)) return false;
-        return Objects.equals(nombre, that.nombre);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nombre);
-    }
+    protected String nombre;
+    protected String descripcion;
+    protected Set<Producto> productos;
 }
--- src/main/java/programacion3/entities/DetallePedido.java
+++ src/main/java/programacion3/entities/DetallePedido.java
@@ -1,6 +1,14 @@
 package programacion3.entities;
 
-import java.util.Objects;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode (of = "producto")
 
 public class DetallePedido {
 
@@ -8,57 +16,12 @@ public class DetallePedido {
     private Double subtotal;
     private Producto producto;
 
-    public DetallePedido() {
-    }
-
-    public DetallePedido(int cantidad, Double subtotal, Producto producto) {
-        this.cantidad = cantidad;
-        this.subtotal = subtotal;
-        this.producto = producto;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public Double getSubtotal() {
-        return subtotal;
-    }
-
-    public void setSubtotal(Double subtotal) {
-        this.subtotal = subtotal;
-    }
-
-    public Producto getProducto() {
-        return producto;
-    }
-
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-
     @Override
-    public String toString() {
+    public String toString(){
         return "DetallePedido{" +
                 "cantidad=" + cantidad +
                 ", subtotal=" + subtotal +
                 ", producto=" + producto.getNombre() +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof DetallePedido that)) return false;
-        return Objects.equals(producto, that.producto);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(producto);
     }
 }
